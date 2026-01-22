34b9eafec9e4fbc24390cb159110008699997ee8
Krzysztof <k.subartowicz@icloud.com>
2026-01-22 20:42:48 +0100

    price calculator, new elements

--- Prop.Api/Services/QuoteCalculator.cs
+++ Prop.Api/Services/QuoteCalculator.cs
@@ -21,8 +21,40 @@ public class QuoteCalculator
         if (quote.HasSecuritySystem)
             basePremium *= 0.9m;
 
+
+
+        if (quote.RoomNumber > 3)
+            basePremium *= 1.1m;
+        else
+        {
+            basePremium *= 1.0m;
+        }
+
+
+        if (quote.ClaimNumber == 0)
+            basePremium *= 0.95m; 
+        else if (quote.ClaimNumber <= 2)
+            basePremium *= 1.1m;
+        else if (quote.ClaimNumber > 2)
+            basePremium *= 1.25m;
+
+        if (quote.TopFloor == true) 
+            basePremium *= 1.1m;
+        else
+        {
+            basePremium *= 1.0m;
+        }
+
+        if (quote.FlammableFloor == true)
+            basePremium *= 1.2m;
+        else
+        {
+            basePremium *= 1.0m;
+        }
+
         if (quote.Client?.Address?.PermanentResidence == true)
             basePremium *= 1.05m;
+
 
         return Math.Round(basePremium, 2);
     }
